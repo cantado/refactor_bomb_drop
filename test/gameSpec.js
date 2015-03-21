b196e38ff2d84828ab40e85431a2f8b45e08e868
unknown <stlaimer@gmail.com>
2015-03-21 10:00:32 +0100

    add more test to get a coverade of 100%

--- test/gameSpec.js
+++ test/gameSpec.js
@@ -7,6 +7,11 @@ camera = {
     position: {}
 };
 bomb = {
+    position: {},
+    rotation: {}
+};
+explosion = {
+    scale: { x: 0, y: 0, z: 0 },
     position: {}
 };
 keyboard = {
@@ -14,9 +19,10 @@ keyboard = {
 };
 bombDropped = false;
 moveDirectionPlane = 0;
-startExplosion = 0;
+startExplosion = false;
 sideSpeed = 0;
 location = {};
+gameIsOver = false;
 
 var assert = require('assert');
 var update = require('../gameLogic');
@@ -88,5 +94,62 @@ describe('Test', function(){
         assert.equal(bombDropped, true);
     });
 
+    it('dropped bomb should fall down', function(){
+        bomb.position.z = 100;
+        bomb.rotation.x = 90;
+        fallSpeed = 1;
+        keyboard.pressed = function(key){ return key==='W' };
+        update();
+        update();
+        assert.equal(bomb.position.z, 98);
+    });
 
+    it('dropped bomb should rotate', function(){
+        bomb.rotation.x = 1;
+        keyboard.pressed = function(key){ return key==='W' };
+        update();
+        update();
+        assert.equal(bomb.rotation.x, 0.98);
+    });
+
+    it('landing bomb should end game', function(){
+        gameOver = function(){ gameIsOver=true; };
+        bomb.position.z = 8;
+        update();
+        assert.equal(gameIsOver, true);
+    });
+
+    it('explosion should getting bigger', function(){
+        startExplosion = true;
+        update(); // +3
+        update(); // +3
+        assert.equal(explosion.scale.x, 6);
+    });
+
+    it('explosion should get a max size', function(){
+        startExplosion = true;
+        explosion.scale.x = 57;
+        update(); // +3
+        update(); // +3
+        assert.equal(explosion.scale.x, 60);
+    });
+
+    it('explosion should hide after max size', function(){
+        startExplosion = true;
+        explosion.scale.x = 57;
+        update(); // +3
+        update(); // +3
+        assert.equal(explosion.position.z, -80);
+    });
+
+    it('after bomb drop pressing A should turn plane left and keep bomb directory', function(){
+        sideSpeed = 1;
+        plane.position.x = 1;
+        bomb.position.x = 1;
+        bombDropped = true;
+        keyboard.pressed = function(key){ return key==='A' };
+        update();
+        assert.equal(plane.position.x, 0);
+        assert.equal(bomb.position.x, 1);
+    });
 });
