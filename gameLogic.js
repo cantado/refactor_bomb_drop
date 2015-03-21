db9f454c45fd83b463eaf93a7f47c391d9f11b94
unknown <stlaimer@gmail.com>
2015-03-21 10:41:20 +0100

    take out working sections and put them into a global function

--- gameLogic.js
+++ gameLogic.js
@@ -1,60 +1,70 @@
-function update(){
-    //animate forward movement of plane camera and bomb
+/**
+ * animate forward movement of plane camera and bomb
+ */
+function animateForwardMovement() {
     plane.position.y += forwardSpeed;
     camera.position.y += forwardSpeed;
-    if(bomb.position.z >= 8)
+    if (bomb.position.z >= 8)
         bomb.position.y += forwardSpeed;
+}
 
-    //control plane
-    if(keyboard.pressed("A"))
+function keyboardEvents() {
+    if (keyboard.pressed("A"))
         moveDirectionPlane = "l";
-    if(keyboard.pressed("S"))
+    if (keyboard.pressed("S"))
         moveDirectionPlane = "n";
-    if(keyboard.pressed("D"))
+    if (keyboard.pressed("D"))
         moveDirectionPlane = "r";
-
-    //reset (reload window)
-    if(keyboard.pressed("R"))
+    if (keyboard.pressed("R"))
         location.reload();
-
-    //drop bomb
-    if(keyboard.pressed("W"))
+    if (keyboard.pressed("W"))
         bombDropped = true;
 
-    //animate bomb falling movement
-    if(bombDropped == true && bomb.position.z >= -8){
-        bomb.position.z -= fallSpeed;
-        bomb.rotation.x -= 0.01;
-    }
     //animate plane side movement
-    if(moveDirectionPlane == "l"){
+    if (moveDirectionPlane == "l") {
         plane.position.x -= sideSpeed;
-        if(bombDropped == false){
+        if (bombDropped == false) {
             bomb.position.x -= sideSpeed;
         }
     }
-    if(moveDirectionPlane == "r"){
+    if (moveDirectionPlane == "r") {
         plane.position.x += sideSpeed;
-        if(bombDropped == false){
+        if (bombDropped == false) {
             bomb.position.x += sideSpeed;
         }
     }
 
-    //control hit detection
-    if(bomb.position.z <= 8 && gameIsOver == false){
-        gameOver();
+    //animate bomb falling movement
+    if (bombDropped == true && bomb.position.z >= -8) {
+        bomb.position.z -= fallSpeed;
+        bomb.rotation.x -= 0.01;
     }
+}
 
-    //animate the explosion
-    if(startExplosion == true && explosion.scale.x < 60){
+function animateExplosion() {
+//animate the explosion
+    if (startExplosion == true && explosion.scale.x < 60) {
         explosion.scale.x += 3;
         explosion.scale.y += 3;
         explosion.scale.z += 3;
     }
 
-    if(startExplosion == true && explosion.scale.x >= 60){
+    if (startExplosion == true && explosion.scale.x >= 60) {
         explosion.position.z = -80;
     }
+}
+
+function hitDetectionBomb() {
+    //control hit detection
+    if (bomb.position.z <= 8 && gameIsOver == false) {
+        gameOver();
+    }
+}
+function update(){
+    animateForwardMovement();
+    keyboardEvents();
+    hitDetectionBomb();
+    animateExplosion();
 }
 
 module.exports = update;
